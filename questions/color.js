da9c9c3ebd2a5ce9e716c6c4dfae0bf18fdb8f5a
Allan V Nandlal <72092423+anandlal1080@users.noreply.github.com>
2020-12-07 22:29:08 -0700

    Created Tables

---
+++ questions/color.js
@@ -0,0 +1,5 @@
+const chalk = require("chalk");
+exports.MANAGER = chalk.underline.redBright("Manager");
+exports.EMPLOYEE = chalk.underline.greenBright("employee");
+exports.DEPARTMENT = chalk.underline.blueBright("Department");
+exports.ROLE = chalk.underline.yellowBright("Role");
